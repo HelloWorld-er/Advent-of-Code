db6ae3b9540dd8120feb42cfd79b1b4f06fc45ed
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-12-05 21:16:09 +0800

    Update 2.py

--- 2023/Question 5/2.py
+++ 2023/Question 5/2.py
@@ -24,10 +24,10 @@ with open("../input/input5.txt", "r") as file_input:
 			except ValueError:
 				pass
 			if len(range_pair) == 2:
-				for seed in range(range_pair[0], range_pair[0] + range_pair[1]):
-					seeds.append({'seed': seed})
+				seeds.append({'seed-start': range_pair[0], 'seed-end': range_pair[0] + range_pair[1] - 1})
 				range_pair = []
 	del content[0]
+	relationship_of_sd = []
 	for type_of_map in content:
 		temp = type_of_map[0].split(' ')
 		temp = [temp[0]]
@@ -36,20 +36,20 @@ with open("../input/input5.txt", "r") as file_input:
 		source = temp[0]
 		destination = temp[1]
 		del type_of_map[0]
+		# print(seeds)
+		relationship_of_sd.append({"convert":(source, destination), "map": []})
 		for each_map in type_of_map:
 			temp = each_map.split(' ')
 			temp = [int(element) for element in temp]
 			destination_start = temp[0]
 			source_start = temp[1]
 			range_length = temp[2]
+			relationship_of_sd[-1]["map"].append(destination_start - source_start)
 			for index_of_seed in range(len(seeds)):
-				if seeds[index_of_seed][source] >= source_start and seeds[index_of_seed][source] < source_start + range_length:
-					seeds[index_of_seed][destination] = seeds[index_of_seed][source] + destination_start - source_start
-				else:
-					if destination not in seeds[index_of_seed]:
-						seeds[index_of_seed][destination] = seeds[index_of_seed][source]
-		if destination == "location":
+				if 
+				
+		if destination == "location-start":
 			break
-seeds.sort(reverse=False, key=lambda element:element["location"])
+seeds.sort(reverse=False, key=lambda element:element["location-start"])
 print(seeds)
-print(seeds[0]["location"])
+print(seeds[0]["location-start"])

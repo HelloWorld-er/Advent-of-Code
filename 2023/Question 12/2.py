bf4c1625fd8fc9efa87231ef8058df4ad491f3ef
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-12-13 19:44:46 +0800

    Update 2.py

--- 2023/Question 12/2.py
+++ 2023/Question 12/2.py
@@ -1,57 +1,84 @@
 total = 0
 symbol_format_info = []
 number_format_info = []
-unknown_symbol_info = []
 present_list = []
+pointers = []
 
 
-def check(symbol_list, number_list, unknown_list, unknown_index):
-	damaged_list = []
-	decide = False
-	for symbol_index in range(len(symbol_list)):
-		if (symbol_list[symbol_index] == '#') or (symbol_index in unknown_index):
-			if decide:
-				damaged_list[-1] += 1
-			else:
-				decide = True
-				damaged_list.append(1)
-		elif decide:
-			decide = False
-	if damaged_list == number_list:
-		return True
-	return False
-
-
-def looping(start, length, unknown_list, symbol_list, number_list):
-	global present_list
-	if len(present_list) == length:
-		if check(symbol_list, number_list, unknown_list, present_list) is True:
-			# print(present_list)
-			print(1)
+def recursion(index, number_index, uncertain_pointers_num, symbol_list, number_list, layer, case_index):
+	global pointers
+	
+	if index >= len(pointers[case_index]):
+		if uncertain_pointers_num == 0 and number_index == len(number_list):
+			# print("reach--------------------------------------------")
 			return 1
 		return 0
-	ans = 0
-	for index in range(start, len(unknown_list)):
-		present_list.append(unknown_list[index])
-		ans += looping(index + 1, length, unknown_list, symbol_list, number_list)
-		del present_list[-1]
-	return ans
 	
+	answer = 0
+	num_of_sharp = 0
+	for symbol in symbol_list[pointers[case_index][index][0]: pointers[case_index][index][1] + 1]:
+		if symbol == '#':
+			num_of_sharp += 1
+	if num_of_sharp == 0:
+		# skip
+		# print("skip-zero")
+		# pointers[case_index][layer + 1] = [[element for element in item] for item in pointers[case_index][layer]]
+		answer += recursion(index + 1, number_index, uncertain_pointers_num + 1, symbol_list, number_list, layer + 1,
+		                    case_index)
+	# print(pointers[index][1] - pointers[index][0] + 1, number_list[number_index]
+	if number_index >= len(number_list):
+		return answer
+	if pointers[case_index][index][1] - pointers[case_index][index][0] + 1 == number_list[number_index]:
+		# print("skip-full")
+		# pointers[case_index][layer + 1] = [[element for element in item] for item in pointers[case_index][layer]]
+		answer += recursion(index + 1, number_index + 1, uncertain_pointers_num, symbol_list, number_list, layer + 1,
+		                    case_index)
+	
+	num = number_list[number_index]
+	# split
+	if pointers[case_index][index][0] > pointers[case_index][index][1] - num:
+		return answer
+	# print(pointers[case_index][layer][index][0], pointers[case_index][layer][index][1], num)
+	for start in range(pointers[case_index][index][0], pointers[case_index][index][1] + 1):
+		# print("split")
+		if start + num - 1 > pointers[case_index][index][1]:
+			# print("break2")
+			break
+		if start > 0 and start > pointers[case_index][index][0] and symbol_list[start - 1] == '#':
+			# print("break2")
+			break
+		if start + num - 1 != pointers[case_index][index][1] and start + num < len(symbol_list) and symbol_list[
+			start + num] == '#':
+			# print("continue")
+			continue
+		# print("start", start)
+		# pointers[case_index][layer + 1] = [[element for element in item] for item in pointers[case_index][layer]]
+		if start + num - 1 >= pointers[case_index][index][1] - 1:
+			answer += recursion(index + 1, number_index + 1, uncertain_pointers_num, symbol_list, number_list,
+			                    layer + 1, case_index)
+		else:
+			original_start = pointers[case_index][index][0]
+			original_end = pointers[case_index][index][1]
+			pointers[case_index][index][0] = start
+			
+			pointers[case_index].insert(index + 1, [element for element in pointers[case_index][index]])
+			pointers[case_index][index][1] = start + num - 1
+			pointers[case_index][index + 1][0] = start + num + 1
+			
+			answer += recursion(index + 1, number_index + 1, uncertain_pointers_num - 1, symbol_list, number_list,
+			                    layer + 1, case_index)
+			
+			pointers[case_index][index][0] = original_start
+			pointers[case_index][index][1] = original_end
+			del pointers[case_index][index + 1]
+	
+	return answer
 
-def loop_order(symbol_list, number_list, unknown_list):
-	global present_list
-	damaged_num = 0
-	for status in symbol_list:
-		if status == '#':
-			damaged_num += 1
-	damaged_all_num = 0
-	for element in number_list:
-		damaged_all_num += element
-	unknown_num = damaged_all_num - damaged_num
-	arrangements = 0
-	present_list = []
-	return looping(0, unknown_num, unknown_list, symbol_list, number_list)
-		
+
+def loop_order(symbol_list, number_list, case_index):
+	global pointers
+	uncertain_pointers_num = len(number_list) - len(pointers[case_index])
+	return recursion(0, 0, uncertain_pointers_num, symbol_list, number_list, 0, case_index)
 
 
 with open("../input/input12.txt", "r") as input_file:
@@ -66,11 +93,26 @@ with open("../input/input12.txt", "r") as input_file:
 				symbol_format_info[-1].append('?')
 			for element in temp[1].split(','):
 				number_format_info[-1].append(int(element))
-		unknown_symbol_info.append([])
-		for symbol_index in range(len(symbol_format_info[-1])):
-			if symbol_format_info[-1][symbol_index] == '?':
-				unknown_symbol_info[-1].append(symbol_index)
+	
 	for case in range(len(symbol_format_info)):
-		total += loop_order(symbol_format_info[case], number_format_info[case], unknown_symbol_info[case])
+		decide = False
+		pointers.append([])
+		element_index = 0
+		while element_index < len(symbol_format_info[case]):
+			element = symbol_format_info[case][element_index]
+			if element == '?' or element == '#':
+				if decide is False:
+					pointers[-1].append([element_index])
+					decide = True
+				element_index += 1
+			else:
+				if decide is True:
+					pointers[-1][-1].append(element_index - 1)
+					decide = False
+				del symbol_format_info[case][element_index]
+		if decide:
+			pointers[-1][-1].append(len(symbol_format_info[case]) - 1)
+	for case in range(len(symbol_format_info)):
+		total += loop_order(symbol_format_info[case], number_format_info[case], case)
 
 print(total)

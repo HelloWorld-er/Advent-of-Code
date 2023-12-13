ddd78ed049edfbaa511ca470cdf04398d13232bb
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-12-13 12:48:47 +0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -29,37 +29,38 @@ def recursion(index, number_index, uncertain_pointers_num, symbol_list, number_l
 		if uncertain_pointers_num == 0 and number_index == len(number_list):
 			return 1
 		return 0
-	
+	print(pointers)
+	print(index)
 	answer = 0
-	group = 0
-	while group <= abs(uncertain_pointers_num) and number_index + group < len(number_list):
-		num = [0]
-		# num_total = 0
-		if uncertain_pointers_num >= 0:
-			for temp_index in range(number_index, number_index + group + 1):
-				num.append(number_list[temp_index] - num[-1])
-				# num_total += number_list[temp_index]
-			# num_total -= group
-		del num[0]
-		positions_num = pointers[index][1] - pointers[index][0] + 1
-		positions_num -= group
-		if positions_num < num[-1]:
+	num_of_sharp = 0
+	for symbol in symbol_list[pointers[index][0]: pointers[index][1] + 1]:
+		if symbol == '#':
+			num_of_sharp += 1
+	new_pointers = [element for element in pointers]
+	if num_of_sharp == 0:
+		# skip
+		answer += recursion(index + 1, number_index, uncertain_pointers_num + 1, symbol_list, number_list, new_pointers)
+	# elif num_of_sharp == number_list[number_index]:
+	# 	answer += recursion(index + 1, number_index + 1, uncertain_pointers_num)
+	num = number_list[number_index]
+	# split
+	for start in range(pointers[index][0], pointers[index][1] + 1 - num):
+		if start > 0 and symbol_list[start - 1] == '#':
 			break
-		argument = 1
-		temp_index = 0
-		while temp_index < len(num):
-			argument *= 
-			
-		if uncertain_pointers_num < 0:
-			group -= 1
-		else:
-			group += 1
+		if start < len(symbol_list) and symbol_list[start + 1] == '#':
+			continue
+		new_pointers[index][0] = start
+		new_pointers.insert(index + 1, [element for element in new_pointers[index]])
+		new_pointers[index][1] = start + num - 1
+		new_pointers[index + 1][0] = start + num
+		answer += recursion(index + 1, number_index + 1, uncertain_pointers_num - 1, symbol_list, number_list, new_pointers)
 	return answer
 
+
 def loop_order(symbol_list, number_list, pointers):
-	new_pointers = [element for element in pointers]
+	# new_pointers = [element for element in pointers]
 	uncertain_pointers_num = len(number_list) - len(pointers)
-	return recursion(0, 0, uncertain_pointers_num, symbol_list, number_list, new_pointers)
+	return recursion(0, 0, uncertain_pointers_num, symbol_list, number_list, pointers)
 
 
 with open("2023/input/input12.txt", "r") as input_file:
@@ -89,5 +90,6 @@ with open("2023/input/input12.txt", "r") as input_file:
 			pointers[-1][-1].append(len(symbol_format_info[case]) - 1)
 	for case in range(len(symbol_format_info)):
 		total += loop_order(symbol_format_info[case], number_format_info[case], pointers[case])
+		print()
 
 print(total)

111f793f0f2bffe4758d245a2506e96a156196c3
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-12-15 21:12:44 +0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,84 +1,142 @@
 total = 0
 symbol_format_info = []
 number_format_info = []
-present_list = []
 pointers = []
 generated_list = []
 sharp_position = []
+original_sharp_position = []
 group_position = []
 group_dict = {}
 
 
 def recursion(index, case_index, layer):
-	global symbol_format_info, generated_list, sharp_position, unknown_position, group_position, group_dict
+	global symbol_format_info, generated_list, sharp_position, unknown_position, group_position, group_dict, original_sharp_position
 	if index == len(sharp_position) - 1:
-		# print(sharp_position)
+		print(1)
 		return len(symbol_format_info[case_index]) - sharp_position[index][1]
 	
 	answer = 0
 	group = group_dict[index]
+	high_bool = 0
 	
 	while True:
+		print(layer, sharp_position)
 		if sharp_position[index + 1][0] - sharp_position[index][1] < 2:
-			break
-		if sharp_position[index][0] != 0 and sharp_position[index][0] - 1 == '#':
-			break
-		if sharp_position[-1][1] >= len(symbol_format_info[case_index]):
-			break
-		ans = 0
-		for element_index in range(sharp_position[index][1], sharp_position[index + 1][0] - 1):
-			if symbol_format_info[case_index][element_index] != '.':
-				ans += 1
+			if high_bool == 0:
+				high_bool = 1
 			else:
 				break
-		if ans == 0:
-			break
-		answer += ans * recursion(index + 1, case_index, layer + 1)
-		
-		if group_position[group][1] - sharp_position[index][1] > 0:
-			for element_index in range(index, len(sharp_position)):
-				sharp_position[element_index][0] += 1
-				sharp_position[element_index][1] += 1
-				if element_index != len(sharp_position) - 1 and sharp_position[element_index + 1][0] - sharp_position[element_index][1] >= 2:
-					break
-		else:
-			if group_position[group][2] is False:
+		if sharp_position[index][0] != 0 and symbol_format_info[case_index][sharp_position[index][0] - 1] == '#':
+			if high_bool == 0:
+				high_bool = 1
+			else:
 				break
-			temp_index = -1
-			for element_index in range(group + 1, len(group_position)):
-				if group_position[element_index][2] is True:
-					temp_bool_empty = True
-					for temp in range(group_position[element_index][0], group_position[element_index][1] + 1):
-						if generated_list[temp] == '#':
-							temp_bool_empty = False
-							break
-					if temp_bool_empty is True:
-						temp_index = element_index
+		if sharp_position[-1][1] >= len(symbol_format_info[case_index]):
+			if high_bool == 0:
+				high_bool = 1
+			else:
+				break
+		if high_bool != 1:
+			ans = 0
+			for element_index in range(sharp_position[index][1], sharp_position[index + 1][0] - 1):
+				if symbol_format_info[case_index][element_index] != '.':
+					if element_index != sharp_position[index][1] and symbol_format_info[case_index][sharp_position[index][0] + element_index - sharp_position[index][1] - 1] == '#':
 						break
-			# print(temp_index, layer)
-			# print(group_position[temp_index])
-			if temp_index == -1:
-				break
-			for element_index in range(index, len(sharp_position)):
-				length = sharp_position[element_index][1] - sharp_position[element_index][0] + 1
-				if length > group_position[group + element_index - index + 1][1] - group_position[group + element_index - index + 1][0] + 1:
-					temp_index = -1
+					ans += 1
+				else:
 					break
-				sharp_position[element_index][0] = group_position[group + element_index - index + 1][0]
-				sharp_position[element_index][1] = sharp_position[element_index][0] + length - 1
-				if group + element_index - index + 1 == temp_index:
-					break
-			if temp_index == -1:
+			if ans == 0:
 				break
+			answer += ans * recursion(index + 1, case_index, layer + 1)
 			
-			group += 1
-			
-			# break
+			temp_num = 0
+			for element_index in range(sharp_position[index][0], len(symbol_format_info[case_index])):
+				if symbol_format_info[case_index][element_index] != '?':
+					temp_num = element_index
+					break
+				temp_num += 1
+			if temp_num >= sharp_position[index][1] - sharp_position[index][0] + 1:
+				print('2--', sharp_position)
+				
+				top_index = index
+				while True:
+					element_index = sharp_position[top_index][1] + ans
+					while element_index < len(symbol_format_info[case_index]):
+						if symbol_format_info[case_index][element_index] != '.':
+							temp_num = 0
+							temp_bool_all_questions = True
+							for temp_index in range(element_index, element_index + sharp_position[top_index][1] - sharp_position[top_index][0] + 1):
+								if symbol_format_info[case_index][temp_index] != '?':
+									temp_bool_all_questions = False
+								if symbol_format_info[case_index][temp_index] == '.':
+									break
+								temp_num += 1
+							if temp_num != element_index + sharp_position[top_index][1] - sharp_position[top_index][0] + 1 - element_index:
+								if temp_bool_all_questions is False:
+									break
+								element_index += 1
+								continue
+							length = sharp_position[top_index][1] - sharp_position[top_index][0] + 1
+							sharp_position[top_index][0] = element_index
+							sharp_position[top_index][1] = element_index + length - 1
+							element_index += 1
+							break
+						element_index += 1
+					if top_index != len(sharp_position) - 1 and sharp_position[top_index + 1][0] - sharp_position[top_index][1] >= 2:
+						break
+					if top_index == len(sharp_position) - 1:
+						break
+					top_index += 1
+		if high_bool >= 1:
+			high_bool = 2
+			print("------------------")
+			# sharp_position[index + 1][0] = original_sharp_position[index + 1][0]
+			# sharp_position[index + 1][1] = original_sharp_position[index + 1][1]
+			# length = sharp_position[index][1] - sharp_position[index][0] + 1
+			# sharp_position[index][0] = group_position[group][0]
+			# sharp_position[index][1] = sharp_position[index][0] + length - 1
+			# if group_position[group][2] is False:
+			# 	break
+			# temp_index = -1
+			# for element_index in range(group + 1, len(group_position)):
+			# 	if group_position[element_index][2] is True:
+			# 		temp_bool_empty = True
+			# 		for temp in range(group_position[element_index][0], group_position[element_index][1] + 1):
+			# 			if generated_list[temp] == '#':
+			# 				temp_bool_empty = False
+			# 				break
+			# 		if temp_bool_empty is True:
+			# 			temp_index = element_index
+			# 			break
+			# # print(temp_index, layer)
+			# # print(group_position[temp_index])
+			# if temp_index == -1:
+			# 	break
+			# for element_index in range(index, len(sharp_position)):
+			# 	length = sharp_position[element_index][1] - sharp_position[element_index][0] + 1
+			# 	if length > group_position[group + element_index - index + 1][1] - group_position[group + element_index - index + 1][0] + 1:
+			# 		temp_index = -1
+			# 		break
+			# 	sharp_position[element_index][0] = group_position[group + element_index - index + 1][0]
+			# 	sharp_position[element_index][1] = sharp_position[element_index][0] + length - 1
+			# 	if group + element_index - index + 1 == temp_index:
+			# 		break
+			# if temp_index == -1:
+			# 	break
+			#
+			# group += 1
+			break
+	
+	# length = sharp_position[index][1] - sharp_position[index][0] + 1
+	# print("original", original_index)
+	
+	# sharp_position[index][0] = original_sharp_position[index][0]
+	# sharp_position[index][1] = original_sharp_position[index][1]
 	return answer
 
 
 def loop_order(case_index):
-	global pointers, generated_list, number_format_info, symbol_format_info, sharp_position, group_position, group_dict
+	global pointers, generated_list, number_format_info, symbol_format_info, sharp_position, group_position, group_dict, original_sharp_position
 	generated_list = []
 	dividing_position = []
 	for index in range(len(number_format_info[case_index])):
@@ -240,6 +298,8 @@ def loop_order(case_index):
 				group_dict[index] = element_index
 				break
 	
+	original_sharp_position = [[element for element in group] for group in sharp_position]
+	
 	print(symbol_format_info[case_index])
 	print(generated_list)
 	print(group_dict)
@@ -250,15 +310,8 @@ def loop_order(case_index):
 with (open("2023/input/input12.txt", "r") as input_file):
 	for line in input_file:
 		temp = line.strip().split(' ')
-		symbol_format_info.append([])
-		number_format_info.append([])
-		for copy in range(5):
-			for element in list(temp[0]):
-				symbol_format_info[-1].append(element)
-			if copy < 4:
-				symbol_format_info[-1].append('?')
-			for element in temp[1].split(','):
-				number_format_info[-1].append(int(element))
+		symbol_format_info.append(list(temp[0]))
+		number_format_info.append([int(element) for element in temp[1].split(',')])
 	
 	for case in range(len(symbol_format_info)):
 		decide = False
@@ -283,4 +336,5 @@ with (open("2023/input/input12.txt", "r") as input_file):
 		print()
 
 print(total)
+print(15684)
 print(525152)

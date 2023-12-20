0d772e844c89678019d8fa17df006490e8f88394
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2023-12-20 17:41:27 +0800

    Update 1.py

---
+++ 2023/Question 14/1.py
@@ -0,0 +1,26 @@
+total = 0
+
+with open("../input/input14.txt", "r") as input_file:
+	info = input_file.readlines()
+	info = [list(line.strip()) for line in info]
+	for straight_vertical_line_index in range(len(info[0])):
+		pointer = 0
+		for element_index in range(len(info)):
+			if info[element_index][straight_vertical_line_index] == '#':
+				pointer = element_index + 1
+			elif info[element_index][straight_vertical_line_index] == 'O':
+				info[element_index][straight_vertical_line_index] = '.'
+				info[pointer][straight_vertical_line_index] = 'O'
+				pointer += 1
+	length = len(info)
+	for line in range(len(info)):
+		num = 0
+		for element in info[line]:
+			if element == 'O':
+				num += 1
+		total += num * length
+		length -= 1
+	# for line in info:
+	# 	print(line)
+
+print(total)
